c231565fa09bfe4c7477478e3ea58e575c55f2a8
Pedro <pedrosjc@outlook.com>
2014-03-04 19:48:51 -0300

    Utilizando JaxRSActivator

---
+++ src/main/java/org/jugvale/call4papers/rest/utils/JaxRSActivator.java
@@ -0,0 +1,9 @@
+package org.jugvale.call4papers.rest.utils;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class JaxRSActivator extends Application {
+
+}
