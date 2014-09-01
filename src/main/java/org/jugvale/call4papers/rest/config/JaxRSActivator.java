da10a6d32a1984d3ddc91f8b08ded73b295d8c25
Pedro Henrique Silva <pedrohenriquesilva@MacBook-Pro-de-Pedro.local>
2014-08-31 22:16:43 -0300

    .

--- src/main/java/org/jugvale/call4papers/rest/config/JaxRSActivator.java
+++ src/main/java/org/jugvale/call4papers/rest/config/JaxRSActivator.java
@@ -3,7 +3,5 @@ package org.jugvale.call4papers.rest.config;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("rest/v1")
-public class JaxRSActivator extends Application {
-
-}
+@ApplicationPath("rest/v1/")
+public class JaxRSActivator extends Application { }
