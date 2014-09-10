bc2b9330faed3afb8a33f8e0183c0c4ecd928b69
jesuino <william.a.siqueira@gmail.com>
2014-09-09 21:19:24 -0300

    Adicionando o nosso amigo activator de novo

---
+++ src/main/java/org/jugvale/call4papers/rest/config/RESTActivator.java
@@ -0,0 +1,9 @@
+package org.jugvale.call4papers.rest.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RESTActivator extends Application{
+
+}
