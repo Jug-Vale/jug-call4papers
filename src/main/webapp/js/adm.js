3de2d4c5adcb3838d83babd4a41f165453f306c0
William Antonio <william.fatecsjc@gmail.com>
2016-03-29 00:41:20 -0300

    eventos na pg de admin

--- src/main/webapp/js/adm.js
+++ src/main/webapp/js/adm.js
@@ -1,8 +1,31 @@
 $(function() {
 	$('.data').mask('00-00-0000 00:00');
+	// com angular td seria mais fácil ..
+	var eventosHtml = "";
+	var eventos = EventoResource.listarTodos();
+	eventos.sort(function(a, b) {
+		var d1 = converteParaData(a.dataFim);
+		var d2 = converteParaData(b.dataFim);
+		return d1 < d2;
+	});
+	var agora = new Date();
+	$.each(eventos, function(key, evt) {
+		
+		eventosHtml += "<li>";
+		if(converteParaData(evt.dataFim) > agora) {
+			eventosHtml += "<span style='color: blue'>"
+		} else {
+			eventosHtml += "<span style='color: red'>"
+		}
+		eventosHtml += evt.nome;
+		eventosHtml += " ( <a href='./rest/evento/"+ evt.id + "/inscritos'>ver inscritos</a> )";
+		eventosHtml += "</span>"
+		eventosHtml +=	"</li>";
+		
+	});
+	$("#todosEventos").append(eventosHtml);
 	
 	$('#btnNovoEvento').click(function() {
-		console.log(haErrosNosForms());
 		if(!haErrosNosForms()) {
 			var evento = {};
 			evento.nome = $("#nome").val();
